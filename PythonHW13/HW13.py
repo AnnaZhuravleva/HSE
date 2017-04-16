77a13cd83d50537cd1da9593f1e2e82b1dedfca8
AnnaZhuravleva <zhuravlevahanna@gmail.com>
2017-04-16 09:36:50 +0300

    dobavil fail

--- PythonHW13/HW13.py
+++ PythonHW13/HW13.py
@@ -1,12 +1,23 @@
 import os, re
 def folder_opening(big_folder):
     names = [item for item in os.listdir(big_folder) if os.path.isfile(item) and re.search('[^.]*\..*?[,._?<>''""!-()].*?',str(item)[::-1])]
-    all_files = []
-    all_files = [item for item in os.listdir(big_folder) if item not in all_files]
-    print('Все файлы и папки:',all_files)
     return len(names)
 print('Найдено',folder_opening('.'), 'файлов, название которых содержит знаки препинания')
-  
 
+def all_files(big_folder):
+    files = [item[::-1] for item in os.listdir(big_folder) if os.path.isfile(item)]
+    all_files = []
+    for item in files:
+            all_files.append((re.sub(u'([^.]*\.)?(.*)', u'\\2', str(item))[::-1]))
+    for item in os.listdir(big_folder):
+        if os.path.isdir(item):
+            all_files.append(item)      
+    all_files_new = []
+    for item in all_files:
+        if item not in all_files_new:
+            all_files_new.append(item)
+    #all_files_new = [item for item in all_files if item not in all_files_new] - почему-то не выполняется условие not in all_files_new
+    return all_files_new
+print('Все файлы:', all_files('.'))
         
     
