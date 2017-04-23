6d035f92143c3b44f26d1e869901f82e1c20f3b9
AnnaZhuravleva <zhuravlevahanna@gmail.com>
2017-04-23 14:51:03 +0300

    dobavil fail

---
+++ PythonHW14/HW14.py
@@ -0,0 +1,13 @@
+#6. в скольких папках встречается несколько файлов с одним и тем же расширением; 
+import os
+number = 0
+for roots, dirs, files in os.walk('.'):
+    names = []
+    for f in files:
+        name = f[::-1].split('.')[0]
+        if name not in names:
+            names.append(name)
+        else:
+            number += 1
+            break
+print(number)
