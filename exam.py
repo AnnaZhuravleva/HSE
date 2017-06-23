1a6eecc082e1a354d7b94ce99a8f09fcfb7764d0
AnnaZhuravleva <zhuravlevahanna@gmail.com>
2017-06-23 15:14:44 +0300

    Add files via upload

--- exam.py
+++ exam.py
@@ -18,8 +18,25 @@ def writing_table(dic):
         for file in dic:
             f.writelines(file + '\t' + str(dic[file]) + '\n')
 
-writing_table(opening_folders(r'C:\Users/student/Desktop/news/'))
+def author_and_topic(folder):
+    path = folder
+    for file in os.listdir(folder):
+        with open ((os.path.join(folder, file))) as f:
+            text = f.read()
+            reg1 = '(content="(.*)" name="author")'
+            reg2 = '(content="(.*)" name="topic")'
+            for i in range (1):
+                for i in re.findall(reg1, text):
+                    author = i[1]
+                for i in re.findall(reg2, text):
+                    topic = i[1]
+    
 
+            
+            
+
+writing_table(opening_folders(r'C:\Users/student/Desktop/news/'))
+author_and_topic(r'C:\Users/student/Desktop/news/')
 
 
     
