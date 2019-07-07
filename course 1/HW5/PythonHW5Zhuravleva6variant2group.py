9780a58ba290ec1f435ee91772372a4814788024
Anna Zhuravleva <zhuravlevhanna@gmail.com>
2019-07-07 16:10:50 +0300

    some changes

--- course 1/HW5/PythonHW5Zhuravleva6variant2group.py
+++ course 1/HW5/PythonHW5Zhuravleva6variant2group.py
@@ -1,20 +1,15 @@
-#Вариант 6. Открыть файл и посчитать процент слов с заглавной буквы
+# Вариант 6. Открыть файл и посчитать процент слов с заглавной буквы
 total = 0
 upletters = 0
-with open(r'C:\Users\Анна\Documents\GitHub\prog\PythonHW5\text.txt','r',encoding='utf8') as f:
+with open(r'C:\Users\qwe\Desktop\HSE\course 1\HW5\text.txt', 'r', encoding='utf8') as f:
     text = f.read()
-    words = text.split()
-    for item in words:
+    for item in text.split():
        total += 1
        for letter in item:
            if letter.isupper():
                upletters += 1
            else:
                continue
-print("Количество слов в тексте: ",total)
-print('Количество слов с заглавной буквы',upletters)
-print("Процент слов в тексте, начинающихся с заглавной буквы: ", upletters/total,'%')
-
-    
-            
-            
+print("Количество слов в тексте: ", total)
+print('Количество слов с заглавной буквы', upletters)
+print("Процент слов в тексте, начинающихся с заглавной буквы: ", upletters/total, '%')
