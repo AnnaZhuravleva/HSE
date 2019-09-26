8b52e7db9f7c5f1c20e1a60bcd0d2aa9d48b2c7c
Anna Zhuravleva <zhuravlevahanna@gmail.com>
2019-09-26 13:22:12 +0300

    Update sem02.py

--- course 4/sem02/sem02.py
+++ course 4/sem02/sem02.py
@@ -7,61 +7,55 @@ import pymorphy2
 
 
 pmm = pymorphy2.MorphAnalyzer()
-vectorizer = TfidfVectorizer()
 
 k = 2.0
 b = 0.75
 
+class BM25:
 
-data = pd.read_csv('collection.csv')
-collection = len(data['question2'])
-docs = [str(doc).split(' ') for doc in data['question2']]
-avg = np.mean([len(doc) for doc in docs]).round(5)
+    def __init__(self):
+        self.data = pd.read_csv('collection.csv')
+        self.collection = self.data['question2']
+        self._length = len(self.data['question2'])
+        self.vectorizer = TfidfVectorizer()
+        self.docs = [str(doc).split(' ') for doc in self.data['question2']]
+        self.avg = np.mean([len(doc) for doc in self.docs]).round(5)
+        self.model = self.vectorizer.fit(self.collection)
+        self.matrix = self.vectorizer.transform(self.collection)
 
+    def bm25(self, doc, query, n, ld):
+        score = 0.0
+        for number, word in enumerate(query):
+            idf = log10((self._length - n[number] + 0.5) / (n[number] + 0.5))
+            tf = doc.count(word)/ld
+            score += idf * (tf * (k + 1.0)) / (tf + k * (1 - b + (b * ld/self.avg)))
+        return round(score, 5)
 
-def normalize():
-    data0 = pd.read_csv('quora_question_pairs_rus.csv')
-    with open('collection.csv', 'w', newline='', encoding='utf8') as w:
-        writer = csv.writer(w, delimiter=',')
-        writer.writerow(['question', 'question2'])
-        for idx, doc in enumerate(docs):
-            words = [w.strip(r',\.\"!–?-\'\"\(\)\"-::—;)(\«\»\W') for w in doc]
-            words = [pmm.normal_forms(word)[0] for word in words]
-            writer.writerow([data0['question2'][idx], ' '.join(words)])
+    def search_iter(self, query):
+        query = query.split()
+        query = [pmm.normal_forms(word)[0] for word in query]
+        n = [sum([1 for doc in self.docs if word in doc]) for word in query]
+        res = []
+        texts = []
+        for i, d in enumerate(self.docs):
+            score = self.bm25(d, query, n, len(d))
+            if score > 0.0 and self.data['question'][i] not in texts:
+                res.append([score, i, self.data['question'][i]])
+                texts.append(self.data['question'][i])
+        res = sorted(res, key=lambda x: x[0], reverse=True)
+        print(res[:5])
 
-
-def bm25(doc, query, n, ld) -> float:
-    score = 0.0
-    for number, word in enumerate(query):
-        idf = log10((collection - n[number] + 0.5) / (n[number] + 0.5))
-        tf = doc.count(word)/ld
-        score += idf * (tf * (k + 1.0)) / (tf + k * (1 - b + (b * ld/avg)))
-    return round(score, 5)
-
-
-def search(query):
-    print(f"Осуществляем поиск по запросу {query}")
-    query = query.split()
-    query = [pmm.normal_forms(word)[0] for word in query]
-    n = [sum([1 for doc in docs if word in doc]) for word in query]
-    res = []
-    texts = []
-    for i, d in enumerate(docs):
-        tmp = bm25(d, query, n, len(d))
-        if tmp > 0.0 and data['question'][i] not in texts:
-            res.append([tmp, i, d, data['question'][i]])
-            texts.append(data['question'][i])
-    res = sorted(res, key=lambda x: x[0], reverse=True)
-    print("Вот 25 наиболее подходящих документов")
-    print("\n".join(
-        [f'text id:{x[1]}, bm25: {x[0]}, text: {x[3]}' for x in res[:25]]))
-
-
-def tf_idf_matrix():
-    return vectorizer.fit_transform(list(data['question2']))
+    def normalize(self):
+        data0 = pd.read_csv('quora_question_pairs_rus.csv')
+        with open('collection.csv', 'w', newline='', encoding='utf8') as w:
+            writer = csv.writer(w, delimiter=',')
+            writer.writerow(['question', 'question2'])
+            for idx, doc in enumerate(self.docs):
+                words = [w.strip(r',\.\"!–?-\'\"\(\)\"-::—;)(\«\»\W') for w in doc]
+                words = [pmm.normal_forms(word)[0] for word in words]
+                writer.writerow([data0['question2'][idx], ' '.join(words)])
 
 
 if __name__ == '__main__':
-    search('рождественские каникулы')
-    print(tf_idf_matrix().shape)
-    pass
+   bm25 = BM25()
+   bm25.search_iter('рождественские каникулы')
