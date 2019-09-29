2782d2d742b0766f8ed185b5b7d7fed16dae3e63
Anna Zhuravleva <zhuravlevahanna@gmail.com>
2019-09-29 17:05:16 +0300

    Update sem02.py

--- course 4/sem02/sem02.py
+++ course 4/sem02/sem02.py
@@ -15,6 +15,7 @@ class Data:
     def __init__(self, k=2.0, b=0.75, top=5000):
         self.k = k
         self.b = b
+        self.top = top
         self.data = pd.read_csv('collection.csv')
         self.collection = self.data['question2'][:top]
         self._length = len(self.collection)
@@ -24,7 +25,7 @@ class Data:
         self.model = self.vectorizer.fit(self.collection)
         self.matrix = self.vectorizer.transform(self.collection)
         self.dictionary = self.vectorizer.get_feature_names()
-        name = re.sub(r'\.', '', f'bm25_matrix_b{self.b}') + '.npy'
+        name = re.sub(r'\.', '', f'matrix_b{self.b}_{top}') + '.npy'
         if os.path.isfile(name):
             self.bm25_matrix = np.load(name)
         else:
@@ -38,7 +39,7 @@ class Data:
             tf = doc.count(word) / ld
             score += idf * (tf * (self.k + 1.0)) / (
                     tf + self.k * (1 - self.b + (self.b * ld / self.avg)))
-        return round(score, 8)
+        return score
 
     def bm25_build_matrix(self, name):
         bm25_matrix = np.zeros(self.matrix.shape)
@@ -48,7 +49,7 @@ class Data:
             for idxd, doc in enumerate(self.docs):
                 bm25_matrix[idxd, idxw] = self.bm25(
                     doc, [word], [n[idxw]], len(doc))
-            if ((idxw / len(self.dictionary)) * 100) % 10 == 0:
+            if int(((idxw / len(self.dictionary)) * 100) % 10) == 0:
                 print(f'{int(idxw / len(self.dictionary) * 100)}%')
         np.save(name, bm25_matrix)
         print('matrix saved')
@@ -95,30 +96,25 @@ class BM25(Data):
         res = sorted(res, key=lambda x: x[1], reverse=True)
         return res[:top]
 
-    def testing(self):
-        test_data = pd.read_csv('quora_question_pairs_rus.csv')
+    def test_time(self):
+        test_data = pd.read_csv('quora_question_pairs_rus.csv')[:self.top]
         answers = list(test_data['is_duplicate'])
-        queries = [j for i, j in
+        queries = [[i, j] for i, j in
                    enumerate(test_data['question1']) if answers[i] == 1]
-        questions1 = test_data['question1']
-        questions2 = test_data['question2']
-        for q1 in queries[:2]:
-            res_iter = [x[0] for x in bm25.search_iter(q1, top=5)]
-            res_matrix = [x[0] for x in bm25.search_matrix(q1, top=5)]
-            print('results', res_iter, res_matrix)
-            accuracy_iter = [[q1, questions1[item], questions2[item],
-                              answers[item]] for item in res_iter]
-            accuracy_matrix = [[q1, questions1[item], questions2[item],
-                                answers[item]] for item in res_matrix]
-            print(accuracy_iter)
-            print(accuracy_matrix)
+        for idx, q1 in enumerate(queries):
+            res_iter = [x[0] for x in bm25.search_iter(q1[1], top=15)]
+            res_matrix = [x[0] for x in bm25.search_matrix(q1[1], top=15)]
+            print(idx, q1[0], q1[1], test_data['question2'][q1[0]])
+            # print('results', res_iter, res_matrix)
+            accuracy_iter = 1 if q1[0] in res_iter else 0
+            accuracy_matrix = 1 if q1[0] in res_matrix else 0
+            print(accuracy_iter, accuracy_matrix)
             print('=================')
         return
 
 
 if __name__ == '__main__':
-    bm25_440000 = BM25(top=440000)
-    bm25_440000.search_iter('каникулы', 10)
-    bm25 = BM25()
-    bm25.testing()
-   # bm25.search_matrix('каникулы', 10)
+    bm25 = BM25(top=10000)
+    bm25.test_time()
+    # print(bm25.search_iter('как я могу быть хорошим геологом?', 6))
+    # bm25.search_matrix('каникулы', 10)
