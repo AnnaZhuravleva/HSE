77e89183c77b4d6d7c5f3531f53ab5419055947b
Anna Zhuravleva <zhuravlevahanna@gmail.com>
2019-10-15 01:18:38 +0300

    Add files via upload

--- course 4/final/mainfile.py
+++ course 4/final/mainfile.py
@@ -1,3 +1,24 @@
-from SearchSystems import FastTextSearch, ElmoSearch, FitModels
+from SearchSystems import *
+from flask import Flask
+from flask import request, render_template
 
-        
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html'), request.args
+
+
+@app.route('/result/')
+def result():
+    tp = request.args
+    if tp['model'] == 2:
+        res = model.fasttext.search(tp['sentence'])
+    res = [(1, 2, 3), (1, 2, 3)]
+    return render_template('result.html', results=res)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
